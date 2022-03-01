a1b52e66b6c61d83ab45e5e4f876af3437b3f2cc
ravindar86 <ravindar.mca@gmail.com>
2022-03-01 06:06:35 +0530

    Initial commit

---
+++ src/test/java/com/discovery/eureka/photoappdiscoveryserver/PhotoAppDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery.eureka.photoappdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
